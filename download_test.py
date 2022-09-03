bc1fc5f56fb35d39dccf578abcd180b08cee88d6
kidclone3 <kid.clone.3@gmail.com>
2022-09-03 15:45:21 +0700

    Add feature: input name and auto compress data to zip file with given name

--- download_test.py
+++ download_test.py
@@ -2,6 +2,10 @@ from bs4 import BeautifulSoup
 import re 
 import wget
 import os
+import shutil
+
+filename = input("File name = ")
+print(filename)
 
 with open('example.html', 'r') as f:
     contents = f.read()
@@ -15,14 +19,20 @@ with open('example.html', 'r') as f:
     if (len(input) != len(output)):
         raise Exception("Input and output files have problem!")
 
-    os.mkdir("data")
+    if len(filename) == 0:
+        filename = "data"
+
+    os.mkdir(filename)    
+    
     n = len(input)
     for i in range(n):
         url1 = input[i]
         url2 = output[i]
         print(url1, url2)
-        wget.download(url1, f'data/i{i+1:02d}.txt')
-        wget.download(url2, f'data/o{i+1:02d}.txt')
+        
+        wget.download(url1, f'{filename}/i{i+1:02d}.txt')
+        wget.download(url2, f'{filename}/o{i+1:02d}.txt')
     print()
     print("DONE!!!")
+    shutil.make_archive(filename, 'zip', root_dir='./')
 # https://he-s3.s3.amazonaws.com/media/hackathon/march-circuits-22/problems/0fc497feab7911ec.txt
