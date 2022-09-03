a312d92c9870306981f9a08b5b0bc203c389e880
kidclone3 <kid.clone.3@gmail.com>
2022-09-03 10:06:55 +0700

    Update v1.0, download exact link for input and output

--- download_test.py
+++ download_test.py
@@ -6,18 +6,20 @@ import os
 with open('example.html', 'r') as f:
     contents = f.read()
 
-    soup = BeautifulSoup(contents, 'lxml')
-    arr = []
-    for link in soup.findAll('a', attrs={'href': re.compile("^https://")}):
-        # print(link.get('href'))
-        arr.append(link.get('href').split('?')[0])
+    soup = BeautifulSoup(contents, 'html.parser')
+    input = [tag.get('href').split('?')[0] for tag in soup.find_all('a', text = re.compile("Input \#.*"))]
+    output =[tag.get('href').split('?')[0] for tag in soup.find_all('a', title = "Correct output for given output")]
+    print(input)
+    print(output)
+    
+    if (len(input) != len(output)):
+        raise Exception("Input and output files have problem!")
 
-    # print(arr)
     os.mkdir("data")
-    n = len(arr)//2
+    n = len(input)
     for i in range(n):
-        url1 = arr[i*2]
-        url2 = arr[i*2+1]
+        url1 = input[i]
+        url2 = output[i]
         print(url1, url2)
         wget.download(url1, f'data/i{i+1:02d}.txt')
         wget.download(url2, f'data/o{i+1:02d}.txt')
