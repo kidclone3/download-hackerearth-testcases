9cdfc35ab712d19185dc98ab6a41c32583e169a8
kidclone3 <kid.clone.3@gmail.com>
2022-09-04 10:58:55 +0700

    Add notification when compress done to OS notification

--- download_test.py
+++ download_test.py
@@ -3,6 +3,7 @@ import re
 import wget
 import os
 import shutil
+import pync
 
 filename = input("File name = ")
 print(filename)
@@ -35,5 +36,5 @@ with open('example.html', 'r') as f:
     print()
     print("DONE!!!")
     shutil.make_archive(f'downloaded/{filename}', 'zip', f'downloaded/{filename}')
-    print("COMPRESS DONE!!!")
+    pync.notify("COMPRESS DONE!!!")
 # https://he-s3.s3.amazonaws.com/media/hackathon/march-circuits-22/problems/0fc497feab7911ec.txt
